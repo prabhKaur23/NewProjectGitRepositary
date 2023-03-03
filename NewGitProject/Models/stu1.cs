b1f4841a96517b6e9fde70975a7d0a1e6b8d4d34
prabhKaur23 <jot23990@gmail.com>
2023-03-03 21:47:12 +0530

    devA new Stu1 class added

---
+++ NewGitProject/Models/stu1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewGitProject.Models
+{
+    public class stu1
+    {
+        public int id { get; set; }
+        public int name { get; set; }
+    }
+}
